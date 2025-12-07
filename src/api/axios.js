c6585d1a991e56e118042d61785ac941e6c82ce0
chhelu123 <chhelurathore773@gmail.com>
2025-12-07 22:57:59 +0530

    Fix package name

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.URLRENDER ? `${import.meta.env.URLRENDER}/api` : '/api',
   headers: {
     'Content-Type': 'application/json'
   }
