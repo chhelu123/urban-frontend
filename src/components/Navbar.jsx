36c4d6cb55300f4712881b0e820f550a6c0ba41c
chhelu123 <chhelurathore773@gmail.com>
2025-12-09 17:30:03 +0530

    Add UrbanWash logo to navbar and home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center space-x-2">
-            <div className="w-10 h-10 bg-gradient-to-br from-primary to-secondary rounded-lg flex items-center justify-center">
-              <span className="text-white font-bold text-xl">U</span>
-            </div>
+            <img src="/logo/Screenshot 2025-12-09 172425.png" alt="UrbanWash Logo" className="h-12 w-auto" />
             <span className="text-2xl font-bold text-gray-900">UrbanWash</span>
           </Link>
 
